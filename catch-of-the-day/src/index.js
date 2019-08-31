12b539f3d65d68fdf54992f9d56463b3ac855e1e
Steven Muniz <steven.muniz@acloud.guru>
2019-08-31 09:42:05 -0500

    Creation of first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { StorePicker } from './StorePicker';
+import StorePicker from './StorePicker';
 
 render(<StorePicker />, document.querySelector('#main'));
