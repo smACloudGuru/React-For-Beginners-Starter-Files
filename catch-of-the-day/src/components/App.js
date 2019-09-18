6a276252bc42176a04ed49bc5f4ae75b5012f3c1
Steven Muniz <steven.muniz@acloud.guru>
2019-09-18 10:20:45 -0500

    Connecting dummy data to push sample fishes to the fishes state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -20,6 +21,12 @@ class App extends React.Component {
     });
   };
 
+  loadSamplesFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -27,7 +34,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSamplesFishes}
+        />
       </div>
     );
   }
