a3f553d46724d2668bfa2814a6d51a927058812c
Steven Muniz <steven.muniz@acloud.guru>
2019-09-18 11:52:11 -0500

    Updating your order state when you use the add to order button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,7 @@ import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
 import { runInThisContext } from 'vm';
+import { timingSafeEqual } from 'crypto';
 
 class App extends React.Component {
   state = {
@@ -29,6 +30,15 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order or update the number in the order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -36,7 +46,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
