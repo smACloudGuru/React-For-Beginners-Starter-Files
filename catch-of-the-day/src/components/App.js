f86cee478ea28c0aea693c62056fff2459ad13f1
Steven Muniz <steven.muniz@acloud.guru>
2019-09-21 10:18:37 -0500

    Setup localStorage to store the quantity of the fish in the store

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,10 +14,24 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match;
+    // First reinstate local storage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorage) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
