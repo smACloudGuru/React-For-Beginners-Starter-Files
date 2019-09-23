b4f26064b64d49b36ca4f39ea7e1fa3cc2ccc2bb
Steven Muniz <steven.muniz@acloud.guru>
2019-09-23 11:36:11 -0500

    Bi-Directional data flow setup using the editFishForm

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,13 +14,12 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match;
-    // First reinstate local storage
+    // first reinstate our localStorage
     const localStorageRef = localStorage.getItem(params.storeId);
-    if (localStorage) {
-      this.setState({
-        order: JSON.parse(localStorageRef)
-      });
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
     }
+
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
@@ -28,6 +27,7 @@ class App extends React.Component {
   }
 
   componentDidUpdate() {
+    console.log(this.state.order);
     localStorage.setItem(
       this.props.match.params.storeId,
       JSON.stringify(this.state.order)
@@ -39,26 +39,31 @@ class App extends React.Component {
   }
 
   addFish = fish => {
-    // 1. Take a copy of the exisiting state
+    // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
     // 2. Add our new fish to that fishes variable
     fishes[`fish${Date.now()}`] = fish;
     // 3. Set the new fishes object to state
-    this.setState({
-      fishes
-    });
+    this.setState({ fishes });
   };
 
-  loadSamplesFishes = () => {
-    this.setState({
-      fishes: sampleFishes
-    });
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. Update that state
+    fishes[key] = updatedFish;
+    // 3. Set that to state
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
   };
 
   addToOrder = key => {
-    // 1. Take a copy of state
+    // 1. take a copy of state
     const order = { ...this.state.order };
-    // 2. Either add to the order or update the number in the order
+    // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
     this.setState({ order });
@@ -83,7 +88,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
-          loadSampleFishes={this.loadSamplesFishes}
+          updateFish={this.updateFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
